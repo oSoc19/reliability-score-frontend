ea8bd8d29e2bd628a1cbc438820efda693cb7e3d
Clément Janssens <clement.janssens.ti@gmail.com>
2019-07-24 13:00:03 +0200

    Footer with credit - readme updated

--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import './SearchPanel.css'
 import DirectionForm from './DirectionForm'
+import HeartIcon from '../images/heart.png'
 
 class SearchPanel extends Component {
     render() {
         return (
-            <div className='content global search-panel'>
-                <h2>Explore the reliability of your train</h2>
+            <Fragment>
+                <div className='content global search-panel'>
+                    <h2>Explore the reliability of your train</h2>
 
-                <DirectionForm CSSClassNames='white-box shadow' />
-            </div>
+                    <DirectionForm CSSClassNames='white-box shadow' />
+                </div>
+
+                <footer>Made with <img src={HeartIcon} alt='love' /> and <a href='https://www.flaticon.com/'>FlatIcon</a> | <a href='https://icomoon.io/'>Icomoon</a> | <a href='https://loading.io/'>Loading.io</a></footer>
+            </Fragment>
         )
     }
 }
